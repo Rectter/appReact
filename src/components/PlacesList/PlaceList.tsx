3904ccffb2131069267fb332fbef0663a2686f1c
rcuamatzi <58236035+rcuamatzi@users.noreply.github.com>
2022-08-05 11:39:52 -0500

    Se quitan imagenes

--- src/components/PlacesList/PlaceList.tsx
+++ src/components/PlacesList/PlaceList.tsx
@@ -40,19 +40,19 @@ const PlaceList: React.FC = () => {
                 imgUrl: 'https://images.pexels.com/photos/460672/pexels-photo-460672.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
                 title: 'London',
                 description: 'Rainy and nice place'
-            },
-            {
-                id: '4',
-                imgUrl: 'https://images.pexels.com/photos/1797161/pexels-photo-1797161.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-                title: 'Rome',
-                description: 'Lots of history in its streets!'
-            },
-            {
-                id: '5',
-                imgUrl: 'https://images.pexels.com/photos/164336/pexels-photo-164336.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-                title: 'Greece',
-                description: 'Perfect beach destination'
             }
+            // {
+            //     id: '4',
+            //     imgUrl: 'https://images.pexels.com/photos/1797161/pexels-photo-1797161.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+            //     title: 'Rome',
+            //     description: 'Lots of history in its streets!'
+            // }
+            // {
+            //     id: '5',
+            //     imgUrl: 'https://images.pexels.com/photos/164336/pexels-photo-164336.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+            //     title: 'Greece',
+            //     description: 'Perfect beach destination'
+            // }
         ]);
     };
 
